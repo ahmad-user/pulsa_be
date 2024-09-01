545a9db571f2d80101dfaf89233823984ff09406
Muhammad Fadilah <1119110006@global.ac.id>
2024-09-01 16:55:17 +0700

    fix default port

--- app.js
+++ app.js
@@ -40,4 +40,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
-app.listen(PORT, () => console.log('Server running at port 5000'));
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
