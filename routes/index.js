886fcc941dc614c68b5917c5d7e6272f20c275a2
Muhammad Fadilah <1119110006@global.ac.id>
2024-09-01 17:04:51 +0700

    testing router

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,14 @@ const path = require('path');
 // const { TambahBanner, TambahService, getBanner, getService } = require('../controller/information.js');
 // const { GetAmount, GetHistory, TambahTransaction, TopUpAmount } = require('../controller/Transactions.js');
 
+const Users = require('../models/UserModels.js');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
 
 const app = express.Router();
 
@@ -15,7 +23,42 @@ const app = express.Router();
 // app.put('/users/:id', VerifyToken, updateUser);
 // app.put('/images/:id', upload.single('profileImage'), VerifyToken, updateImage);
 // app.post('/register', Register);
-// app.post('/login', Login);
+app.post('/login', async(req, res)=>{
+
+    try{
+        const user = await Users.findAll({
+            where:{
+                email: req.body.email,
+            }
+        });
+        const match = await bcrypt.compare(req.body.password, user[0].password);
+        if(!match){
+
+         return res.status(401).json({
+          status: 103,
+          message: "password salah",
+          data: null
+        });
+      }
+        const userId = user[0].id;
+        const email = user[0].email;
+        const password = user[0].password;
+        const accessToken = jwt.sign({userId, email, password}, process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn: '12h'
+    });
+    res.status(200).json({ 
+      status: 0,
+      message: "Login Sukses",
+      data: accessToken
+    });
+    } catch(error){
+        res.status(404).json({
+          status: 102,
+          message: 'Parameter Email Tidak Sesuai Format',
+          data : null
+        });
+    }
+});
 // app.delete('/logout', Logout);
 
 //Banner
