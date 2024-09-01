b739524db381bd3fdc80e43c464a439e9129468b
Muhammad Fadilah <1119110006@global.ac.id>
2024-09-01 17:13:55 +0700

    fix routes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,8 @@
 const express = require('express');
-const path = require('path');
-// const { getUsers, Register, Login, Logout, updateUser, getUserById, deleteUser, updateImage, upload } = require('../controller/User.js');
+const { User } = require('../controller');
 // const  VerifyToken  = require('../middleware/VerifyToken.js');
 // const { TambahBanner, TambahService, getBanner, getService } = require('../controller/information.js');
 // const { GetAmount, GetHistory, TambahTransaction, TopUpAmount } = require('../controller/Transactions.js');
-
-const Users = require('../models/UserModels.js');
-const validator = require('validator');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const multer = require('multer');
-const fs = require('fs');
 
 
 const app = express.Router();
@@ -22,42 +14,7 @@ const app = express.Router();
 // app.put('/users/:id', VerifyToken, updateUser);
 // app.put('/images/:id', upload.single('profileImage'), VerifyToken, updateImage);
 // app.post('/register', Register);
-app.post('/login', async(req, res)=>{
-
-    try{
-        const user = await Users.findAll({
-            where:{
-                email: req.body.email,
-            }
-        });
-        const match = await bcrypt.compare(req.body.password, user[0].password);
-        if(!match){
-
-         return res.status(401).json({
-          status: 103,
-          message: "password salah",
-          data: null
-        });
-      }
-        const userId = user[0].id;
-        const email = user[0].email;
-        const password = user[0].password;
-        const accessToken = jwt.sign({userId, email, password}, process.env.ACCESS_TOKEN_SECRET,{
-        expiresIn: '12h'
-    });
-    res.status(200).json({ 
-      status: 0,
-      message: "Login Sukses",
-      data: accessToken
-    });
-    } catch(error){
-        res.status(404).json({
-          status: 102,
-          message: 'Parameter Email Tidak Sesuai Format',
-          data : null
-        });
-    }
-});
+app.post('/login', User.Login);
 // app.delete('/logout', Logout);
 
 //Banner
@@ -73,9 +30,5 @@ app.post('/login', async(req, res)=>{
 // app.post('/topUp', VerifyToken, TopUpAmount);
 // app.get('/transaction', VerifyToken, GetAmount);
 // app.get('/history', VerifyToken, GetHistory);
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/index.html'));
-  });
 
 module.exports = app;
