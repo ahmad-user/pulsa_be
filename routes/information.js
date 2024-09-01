1789bf7e803e370377893060134018e471d91c55
Muhammad Fadilah <1119110006@global.ac.id>
2024-09-01 18:20:52 +0700

    add transactions

--- routes/information.js
+++ routes/information.js
@@ -1,5 +1,31 @@
 const express = require("express");
+const VerifyToken = require("../middleware/VerifyToken.js");
+const Banner = require("../models/BannerModels.js");
+const Service = require("../models/ServiceModels.js");
 
 const app = express.Router();
+
+app.post("/banner", VerifyToken, async (req, res) => {
+  const { banner_name, banner_image, description } = req.body;
+  try {
+    await Banner.create({
+      banner_name: banner_name,
+      banner_image: banner_image,
+      description: description,
+    });
+    res.status(200).json({
+      status: 0,
+      message: "Tambah Banner Berhasil",
+      data: null,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: 102,
+      message: "Terjadi Kesalahan Pada Server",
+      data: null,
+    });
+  }
+});
 
 module.exports = app;
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,6 +1,29 @@
 const express = require("express");
+const Transaction = require("../models/TransaksiModels.js");
+const Service = require("../models/ServiceModels.js");
+const Users = require("../models/UserModels.js");
+const VerifyToken = require("../middleware/VerifyToken.js");
 
 const app = express.Router();
 
+app.get("/", VerifyToken, async (req, res) => {
+  try {
+    const users = await Users.findAll({
+      attributes: ["top_up_amount"],
+    });
+    res.status(200).json({
+      status: 0,
+      message: "Get Balance Berhasil",
+      data: {
+        balance: users[0].top_up_amount,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 102,
+      message: "Terjadi Kesalahan pada Server",
+    });
+  }
+});
 
 module.exports = app;
